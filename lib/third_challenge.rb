ed674f2ce5b646ae70f8aec8b29f3e3ff24ac3b6
John Mark Redding <johnmarkredding@gmail.com>
2018-11-11 00:55:47 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -27,7 +27,7 @@ def third_challenge
       :matriarch => {
         #your key/value pairs here
         :name => "Lord Montague",
-        :age => "53"
+        :age => "51"
         },
       :heroine => {
         #your key/value pairs here
